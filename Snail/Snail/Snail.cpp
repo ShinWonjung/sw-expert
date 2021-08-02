b099a620d0b9280ae33c112c6da2e69f8bdff46d
ShinWonjung <dnjswjd1204@gmail.com>
2021-08-02 11:09:28 +0900

    3만 정답이 나옴,,,

---
+++ Snail/Snail/Snail.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	int** result = new int*[t];
+	for (int i = 0; i < t; i++) {
+		int n, max;
+		cin >> n;
+		result[i] = new int[n * n];
+		int** arr = new int*[n];
+		for (int j = 0; j < n; j++) arr[j] = new int[n] {0};
+		max = n - 1;
+		int j = 0;
+		int num = 0;
+		while (1) {
+			for (int g = j; g <= max - j; g++) {
+				arr[j][g] = ++num;
+			}
+			if (n % 2 == 1 && arr[n / 2][n / 2] != 0) break;
+			for (int g = 1; g <= max - j; g++) {
+				arr[j + g][max - j] = ++num;
+			}
+			for (int g = 1; g <= max - j; g++) {
+				arr[max - j][max - g] = ++num;
+			}
+			for (int g = 1; g < max - j; g++) {
+				arr[max - g][j] = ++num;
+			}
+			if (n % 2 == 0 && arr[n / 2 - 1][n / 2] == 0) break;
+			j++;
+		}
+		num = 0;
+		for (int g = 0; g < n; g++) {
+			for (int p = 0; p < n; p++) {
+				result[i][num] = arr[g][p];
+				num++;
+			}
+		}
+		for (int g = 0; g < n; g++) delete[] arr[g];
+		delete[] arr;
+	}
+
+	for (int i = 0; i < t; i++) {
+		cout << '#' << i + 1 << "\n";
+		int s = 0;
+		while (1) {
+			if (result[i][s] != s+1) {
+				break;
+			}
+			s++;
+		}
+		int num = 0;
+		for (int j = 0; j < s; j++) {
+			for (int g = 0; g < s; g++) {
+				cout << result[i][num] << ' ';
+				num++;
+			}
+			cout << "\n";
+		}
+		delete[] result[i];
+	}
+	delete[] result;
+	return 0;
+}
